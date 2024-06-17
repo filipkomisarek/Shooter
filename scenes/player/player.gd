7356082775487a31d86c1e3ed87a3152a8ce0f86
Filip Komisarek <filip.komisarek@vividgames.com>
2024-06-17 22:45:59 +0200

    change movement to move and slide for Player

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,10 +1,11 @@
-extends Node2D
+extends CharacterBody2D
 
 func _process(delta):
 	
 	# input movement
 	var direction = Input.get_vector("left", "right", "up", "down")
-	position += direction * 500 * delta
+	velocity = direction * 500
+	move_and_slide()
 	
 	# laser shooting input
 	if Input.is_action_pressed("primary action"):
