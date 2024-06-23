8778525bff077161ea8b368f06ecbd22b1ba2c91
Filip Komisarek <filip.komisarek@vividgames.com>
2024-06-23 15:12:25 +0200

    add 2 delays

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,5 +1,8 @@
 extends CharacterBody2D
 
+var can_laser: bool = true
+var can_grenade: bool = true
+
 func _process(_delta):
 	
 	# input movement
@@ -8,9 +11,16 @@ func _process(_delta):
 	move_and_slide()
 	
 	# laser shooting input
-	if Input.is_action_pressed("primary action"):
+	if Input.is_action_pressed("primary action") and can_laser:
 		print('shoot laser')
+		can_laser = false
+		$Timer.start()
 		
 	# grenade shooting input
-	if Input.is_action_pressed("secondary action"):
+	if Input.is_action_pressed("secondary action") and can_grenade:
 		print('shoot grenade')
+		can_grenade = false
+
+
+func _on_timer_timeout():
+	can_laser = true
