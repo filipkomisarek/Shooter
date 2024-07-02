07e44171f70831442de9a2c63ee7a84b7581a728
Filip Komisarek <filip.komisarek@vividgames.com>
2024-07-02 07:04:13 +0200

    add laser.emit and grenade.emit
    deleted print

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -1,9 +1,15 @@
 extends Node2D
 
 
-func _on_area_2d_body_entered(_body):
-	print("body has entered")
+
+func _on_gate_player_entered_gate(body):
+	print("player has entered gate")
+	print(body)
 
 
-func _on_area_2d_body_exited(_body):
-	print("body has exited")
+func _on_player_laser():
+	print("laser from level")
+
+
+func _on_player_grenade():
+	print("grenade from level")
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -3,6 +3,9 @@ extends CharacterBody2D
 var can_laser: bool = true
 var can_grenade: bool = true
 
+signal laser
+signal grenade
+
 func _process(_delta):
 	
 	# input movement
@@ -12,15 +15,15 @@ func _process(_delta):
 	
 	# laser shooting input
 	if Input.is_action_pressed("primary action") and can_laser:
-		print('shoot laser')
 		can_laser = false
 		$Timer.start()
+		laser.emit()
 		
 	# grenade shooting input
 	if Input.is_action_pressed("secondary action") and can_grenade:
-		print('shoot grenade')
 		can_grenade = false
 		$GrenadeReloadTimer.start()
+		grenade.emit()
 
 
 func _on_timer_timeout():
